ee6a5bfe61dfe721f94ecb8697403929dd0e30c6
Joseph Spedale <jspedale@gmail.com>
2017-02-08 15:17:06 -0500

    Added react-router-dom to project.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import App from './App';
 import './index.css';
 
+const Root = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+    </div>
+  </Router>
+)
+
 ReactDOM.render(
-  <App />,
+  <Root />,
   document.getElementById('root')
 );
