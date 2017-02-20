e8a8a615621d3cbc621aa501359096f6576fca65
Joe Spedale <jspedale@gmail.com>
2017-02-20 11:15:08 -0500

    Added basic layout for choosing donation amount.

--- src/Donate.js
+++ src/Donate.js
@@ -2,13 +2,30 @@ import React, { Component } from 'react';
 import './Donate.css';
 import Nav from './Nav';
 import Footer from './Footer';
+import hero from './america-hero.jpg';
 class Donate extends Component {
   render() {
+  const amounts = [5, 25, 50, 75, 100, 300];
     return(
       <div>
         <Nav />
-        <div className="placeholder">donate please</div>
-        <Footer />
+        <div className="container">
+          <div className="donate-box">
+            <div className="donate-header">
+              Contribute to Candidate today!
+            </div>
+            <div className="amounts-box">
+              {amounts.map(item => <div className="amount">{item}</div>)}
+              <div className="choose-amount">
+                Choose your amount: $<input className="choose-input" type="number"></input>
+              </div>
+            </div>
+            <div className="continue">
+              <div><button>continue</button></div>
+            </div>
+          </div>
+          <Footer />
+        </div>
       </div>
     )
   }
